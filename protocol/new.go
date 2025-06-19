816f40fcd2b71d5ef30937f9e7b6239eba8111ec
vOmarred11 <canziansonia@gmail.com>
2025-06-19 09:33:44 +0200

    added
    added because some values cannot be sent

---
+++ protocol/new.go
@@ -0,0 +1,11 @@
+package protocol
+
+// New is every new value incoming
+type New struct {
+	// Type defines the type of the incoming value
+	Type any
+}
+
+func (n New) New(a any) any {
+	return a
+}
