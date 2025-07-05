ec65d3b217e8bf6745157286768c4eec329ab13f
vOmarred11 <canziansonia@gmail.com>
2025-07-05 18:31:35 +0200

    fixed time file

--- server/timeout/time.go
+++ server/timeout/time.go
@@ -5,12 +5,12 @@ import "time"
 // Time defines any duration sent by server
 type Time struct {
 	// Stamp is the time when you join the server
-	Stamp time.Duration
+	Stamp time.Time
 	// Sleep is the additional duration time when you join the server
 	Sleep time.Duration
 }
 
-func (t Time) TimeStamp() time.Duration {
+func (t Time) TimeStamp() time.Time {
 	return t.Stamp
 }
 func (t Time) TimeSleep() time.Duration {
