8072f5a537d94dc356cfc3bd0e4bacc145fa85d2
vOmarred11 <canziansonia@gmail.com>
2025-07-20 11:31:01 +0200

    fixed permission message

--- permission/cmd/cmd.go
+++ permission/cmd/cmd.go
@@ -1,11 +1,15 @@
 package cmd
 
+import "github.com/vOmarred11/VakuyProtocol/player"
+
 // PermissionCommand defines the command permission for each permission level
 type PermissionCommand struct {
 	// Level is the permission level
 	Level uint8
 	// Commands is a slice of commands that will have a permission
 	Commands []string
+	// HasPermission defines if the player has permissions
+	HasPermission bool
 }
 
 func (p PermissionCommand) PermissionCommandLevel() uint8 {
@@ -13,4 +17,11 @@ func (p PermissionCommand) PermissionCommandLevel() uint8 {
 }
 func (p PermissionCommand) PermissionCommandCommands() []string {
 	return p.Commands
+}
+func (p PermissionCommand) PermissionCommandHasPermission() bool {
+	pp := player.Player{}
+	if p.HasPermission == false {
+		pp.Message("You have no permission to use this command")
+	}
+	return p.HasPermission
 }
