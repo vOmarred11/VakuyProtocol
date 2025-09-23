a5d1fb81a7dca660db617d580a30501d59c2bd3c
vOmarred11 <canziansonia@gmail.com>
2025-09-23 15:41:17 +0200

    Update pos.go

--- permission/area/pos.go
+++ permission/area/pos.go
@@ -1,15 +1,21 @@
 package area
 
+import "github.com/sandertv/gophertunnel/minecraft/protocol"
+
 type Position struct {
-	AreaPosStart Area
-	AreaPosEnd   Area
+	AreaPosStart float32
+	AreaPosEnd   float32
 	error
 }
 
-func (pos Position) PositionAreaPosStart() Area {
+func (pos Position) PositionAreaPosStart() float32 {
+	x := protocol.BlockPos{}.X()
+	pos.AreaPosStart = float32(x) + 1
 	return pos.AreaPosStart
 }
-func (pos Position) PositionAreaPosEnd() Area {
+func (pos Position) PositionAreaPosEnd() float32 {
+	x := protocol.BlockPos{}.X()
+	pos.AreaPosStart = float32(x) - 1
 	return pos.AreaPosEnd
 }
 func (pos Position) Error() error {
