1a55fb4e5713e296758dd71d251b91b8f2342295
vOmarred11 <canziansonia@gmail.com>
2025-09-23 15:36:01 +0200

    Update new.go

--- protocol/new.go
+++ protocol/new.go
@@ -10,5 +10,14 @@ func (n New) NewType() any {
 	return n.Type
 }
 func (n New) New(a any) any {
+	a = a.(New)
+	proto := Proto{}
+	x := proto.Int()
+	if a == x {
+		panic("failed to create a new type: value shouldn't be type int")
+	}
+	if a == nil {
+		panic("failed to create a new type: value shouldn't be type error")
+	}
 	return a
 }
